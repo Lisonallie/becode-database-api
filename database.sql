47dfd81bbd84900d2534745837ef378891dc2381
Lisonallie <allisonvanlinden@gmail.com>
2019-08-09 10:02:03 +0200

    fix some limits

--- database.sql
+++ database.sql
@@ -3,8 +3,8 @@ CREATE DATABASE Notepad;
 CREATE TABLE Note
 (
     id              INT                     NOT NULL      PRIMARY KEY AUTO_INCREMENT,
-    author         VARCHAR (70)    DEFAULT NULL,
-    title          VARCHAR (255)   DEFAULT NOT NULL,
-    text_entry    TEXT            DEFAULT NULL,
+    author          VARCHAR (140)    DEFAULT NULL,
+    title           VARCHAR (500)   DEFAULT NOT NULL,
+    text_entry      TEXT            DEFAULT NULL,
     date_posted     DATE            DEFAULT NULL
 );
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+require ("config.php");
+
+//create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+?>
