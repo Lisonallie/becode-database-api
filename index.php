55aa1a979494aa05494925090d8b1166ae2809da
Lisonallie <allisonvanlinden@gmail.com>
2019-08-12 14:51:57 +0200

    add json results and give array more semantic name

--- index.php
+++ index.php
@@ -8,36 +8,34 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-echo "Connected successfully";
-
 //sanitize
 $author = filter_var($_GET['author'], FILTER_SANITIZE_STRING);
 $title = filter_var($_GET['title'], FILTER_SANITIZE_STRING);
 $text = filter_var($_POST['text'], FILTER_SANITIZE_STRING);
 
-$errors = array();
+$feedback = array();
 
 //validate
 if (empty($title)) {
-    $errors['title'] = "Title may not be empty.";
+    $feedback['title'] = "Title may not be empty.";
 } 
 
 if (empty($text)) {
-    $errors['text'] = "Text may not be empty.";
+    $feedback['text'] = "Text may not be empty.";
 } 
 
-//insert into the database if the previous conditions have been met and there's no errors.
-if (count($errors) > 0) {
-    echo "Sorry, there's an error: ";
-    $json_errors = json_encode($errors);
-    exit;
+//insert into the database if the previous conditions have been met and there's no feedback.
+if (count($feedback) > 0) {
+    $feedback['errors'] = "There are errors.";
 } else {
     $insert = "INSERT INTO note (author, text_entry, title) VALUES ('$author', '$text', '$title')";
     if (mysqli_query($conn, $insert)) {
-        echo "Congratulations.";
+        $feedback['success'] = "Your input has been accepted.";
     } else {
-        echo "Not submitted.";
+        $feedback['fail'] = "Your input has not been submitted.";
     }
 }
+$feedback_result_json = json_encode($feedback);
+echo $feedback_result_json;
 
 ?>
