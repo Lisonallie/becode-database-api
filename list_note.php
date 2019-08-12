f5edeb13f9ad94972022e8452563b0ebe09311a8
Lisonallie <allisonvanlinden@gmail.com>
2019-08-12 16:45:51 +0200

    add experimental list code

--- list_note.php
+++ list_note.php
@@ -1,12 +1,11 @@
 <?php
-
 //create connection
 require "info.php";
 
 //sanitize and declare variable fields
 $author = filter_var($_GET['author'], FILTER_SANITIZE_STRING);
 $title = filter_var($_GET['title'], FILTER_SANITIZE_STRING);
-$text = filter_var($_POST['text'], FILTER_SANITIZE_STRING);
+$text = filter_var($_GET['text'], FILTER_SANITIZE_STRING);
 
 //create array for json responses
 $feedback = array();
@@ -18,23 +17,24 @@ if (empty($title)) {
 
 if (empty($text)) {
     $feedback['text'] = "Text may not be empty.";
-} 
+}
 
 //insert into the database if the previous conditions have been met and there's no feedback.
 if (count($feedback) > 0) {
     $feedback['errors'] = "There are errors.";
 } else {
-    $insert = "INSERT INTO note (author, text_entry, title) VALUES ('$author', '$text', '$title')";
-    if (mysqli_query($conn, $insert)) {
-        $feedback['success'] = "Your input has been accepted.";
+    $list = "SELECT * FROM note ORDER BY title";
+    if (mysqli_query($conn, $list)) {
+        $feedback['success'] = $list/*"Your request has been accepted."*/;
     } else {
-        $feedback['fail'] = "Your input has not been submitted.";
+        $feedback['fail'] = "Your request has not been accepted.";
     }
 }
 
 // display errors resulted in JSON format.
 $feedback_result_json = json_encode($feedback);
 echo $feedback_result_json;
+echo $feedback['success'];
 
 //end connection
 $conn = null;
