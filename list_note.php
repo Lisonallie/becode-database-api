eb73436de349b6740e4b12fc3704fb0a35a6b13e
Lisonallie <allisonvanlinden@gmail.com>
2019-08-27 11:06:23 +0200

    update API to work practically

--- list_note.php
+++ list_note.php
@@ -15,20 +15,19 @@ $list = "SELECT * FROM note ORDER BY title";
 $query = mysqli_query($conn, $list);
 
 //create json-changeable variable request of all rows in the table
-$display_rows = mysqli_fetch_all($query, MYSQLI_ASSOC);
+$display_rows = array( mysqli_fetch_all($query, MYSQLI_ASSOC) );
 
 //display the resulting query fetch in json format
 echo json_encode($display_rows);
 
 //create conditions for cases of success and fail connection and query requests
 if ($query) {
-    $feedback['confirm'] = "Your query has been successfully displayed.";
 } else {
     $feedback['deny'] = "Your query could not be displayed:" . $conn -> error;
+    echo json_encode($feedback);
 }
 
 // display errors resulted in JSON format.
-echo json_encode($feedback);
 
 //end connection
 $conn = null;
--- update_note.php
+++ update_note.php
@@ -4,11 +4,9 @@ header('Access-Control-Allow-Origin: *');
 require "info.php";
 
 //sanitize and declare variable fields
-$author = filter_var($_GET['author'], FILTER_SANITIZE_STRING);
-$title = filter_var($_GET['title'], FILTER_SANITIZE_STRING);
+$text = filter_var($_GET['text'], FILTER_SANITIZE_STRING);
 
-$new_author = filter_var($_GET['new_author'], FILTER_SANITIZE_STRING);
-$new_title = filter_var($_GET['new_title'], FILTER_SANITIZE_STRING);
+$newtext = filter_var($_GET['newtext'], FILTER_SANITIZE_STRING);
 
 //create array for json responses
 $feedback = array();
@@ -17,18 +15,12 @@ $feedback = array();
 
     // below comment gets rid of all fields in table that are not filled in
     // "UPDATE note SET author = '".$new_author."', title = '".$new_title."', text_entry = '".$new_text."' WHERE author = '".$author."' OR title = '".$title."' OR text_entry = '".$text."'";
-$update_author = "UPDATE note SET author = '".$new_author."' WHERE author = '".$author."'";
-$update_title = "UPDATE note SET title = '".$new_title."' WHERE title = '".$title."'";
+$update_text = "UPDATE note SET text_entry = '".$newtext."' WHERE text_entry = '".$text."'";
 
 //separate each query so it doesn't delete the rest of the table
-if (mysqli_query($conn, $update_author)) {
-    $feedback['success'] = "The record has been changed.";
-} else {
-    $feedback['fail'] = "The record has not been changed.";
-}
-
-if (mysqli_query($conn, $update_title)) {
-    $feedback['success'] = "The record has been changed.";
+if (mysqli_query($conn, $update_text)) {
+    var_dump($text);
+    var_dump($newtext);
 } else {
     $feedback['fail'] = "The record has not been changed.";
 }
